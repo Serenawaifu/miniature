532bb5bf5a9174da0c32849622141b042487e599
Sparkie <78498155+Serenawaifu@users.noreply.github.com>
2025-06-07 19:10:02 +0530

    Create CardPlaceholder.tsx

---
+++ components/CardPlaceholder.tsx
@@ -0,0 +1,7 @@
+export default function CardPlaceholder() {
+  return (
+    <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-4 h-40 flex items-center justify-center border border-gray-200 dark:border-gray-700 animate-pulse">
+      <div className="w-16 h-24 bg-gray-200 dark:bg-gray-700 rounded" />
+    </div>
+  );
+}
