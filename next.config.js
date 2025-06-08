349ece7cbeb174015a8fd5d163b5b47b37d808a0
Sparkie <78498155+Serenawaifu@users.noreply.github.com>
2025-06-08 13:40:39 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    domains: [
+      'static.hianime.to',
+      'azuki.com',
+      'lh3.googleusercontent.com',
+      'images.unsplash.com',
+    ],
+  },
+  experimental: {
+    appDir: true,
+  },
+};
